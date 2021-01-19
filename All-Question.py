b5cc1e4417b1a1c0ed86d199c94b690233c88ca0
Amansingh1811 <amansingh18112000@gamil.com>
2021-01-19 07:43:18 -0500

    Programing Questions

---
+++ All-Question.py
@@ -0,0 +1,5 @@
+Q(1)-Given an unsorted array which has a number in the majority(a number appears more than 50% in the array)find that number.
+Q(2)-how to detect a cycle in singly linked list ?
+Q(3)-How to merge two shorted linked list, write a program in your favorite programing language.
+Q(4)-Write a program  which checks if two strings are anagram or not ?
+Q(5)-How to swap two number without using a temp variable, write a code which is free from integer overflow?
