aa5233d37530b58e15b1cf76e4c2a9814ac7d183
Amansingh1811 <amansingh18112000@gamil.com>
2021-01-20 02:15:23 -0500

    initial commit completed

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-print("Hello word")
+import majority_number as m
+arr = [1, 1, 5, 1, 3, 5, 1]
+n = len(arr)
+M = m.findMajority()
+
+print("hello")
+# findMajority(arr, n)
---
+++ majority_number.py
@@ -0,0 +1,21 @@
+def findMajority():
+    arr = [1, 1 , 5, 1, 3, 5, 1]
+    n = len(arr)
+    maxcount = 0 
+    index = -1 
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            if(arr[i] == arr[j]):
+                count += 1
+        if (count > maxcount):
+            maxcount = count
+            index = i
+    if (maxcount > n//2):
+        print("Majority number is - ",arr[index])
+    
+    else:
+        print("no Majority Element")
+        
+        
+
